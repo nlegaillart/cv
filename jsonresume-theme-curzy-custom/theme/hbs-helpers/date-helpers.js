52021da5a8fbdacbeec9591b109dc36039ec4829
n <n@tourmentine.com>
2018-11-06 08:03:50 +0100

    locale date

--- jsonresume-theme-curzy-custom/theme/hbs-helpers/date-helpers.js
+++ jsonresume-theme-curzy-custom/theme/hbs-helpers/date-helpers.js
@@ -1,9 +1,9 @@
 const moment = require('moment');
 
 const dateHelpers = {
-  MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY'),
-  Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
-  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY')
+  MY: date => moment(date.toString(), ['YYYY-MM-DD']).locale("fr").format('MMMM YYYY'),
+  Y: date => moment(date.toString(), ['YYYY-MM-DD']).locale("fr").format('YYYY'),
+  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).locale("fr").format('D MMMM YYYY')
 };
 
 module.exports = { dateHelpers };
